dc3d0b67cf9c1337e388dfe91a96fea5e7e2be54
Triin <trinzaa@gmail.com>
2023-07-04 23:33:40 +0300

    kodutöö 1

---
+++ kodutoo1/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="nupp">Nupp</button>
+      <p className="sininetekst">Sinine tekst</p>
+      <p><span className="rohelinetekst">Roheline tekst</span></p>
+      <img className="pilt" src="https://t3.ftcdn.net/jpg/02/34/52/94/360_F_234529463_eAk2OLMGkaXqVmH2B9QUkdyCHvId9hCd.jpg" alt="pilt"/>
+
+
+      
+    </div>
+  );
+}
+
+export default App;
