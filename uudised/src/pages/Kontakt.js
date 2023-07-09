c26663f6418dfe48b22ab22583eeca000cd29207
Triin <trinzaa@gmail.com>
2023-07-09 12:55:41 +0300

    useState, kontaktandmete näitamine

--- uudised/src/pages/Kontakt.js
+++ uudised/src/pages/Kontakt.js
@@ -1,22 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Kontakt() {
+  const [n2itaTelKristiine, muudaN2itaTelKristiine] = useState(false);
+  const [n2itaTelYlemiste, muudaN2itaTelYlemiste] = useState(false);
+  const [n2itaTelTasku, muudaN2itaTelTasku] = useState(false);
+
   return (
     <div>
         <div>See on kontaktide leht, nähtav localhot:3000/kontakt aadressil</div>
         <div>Võta meiega ühendust:</div>
         <br/>
-        <div>Tallinna kontor</div>
-        <div>Kuskil kesklinnas</div>
-        <div>+345654456</div>
+        <div onClick={() => muudaN2itaTelKristiine(!n2itaTelKristiine)}>Kristiine keskus</div>
+        <div>Endla 45</div>
+       { n2itaTelKristiine && <div>+345654456</div>}
         <br/>
-        <div>Tartu kontor</div>
-        <div>Kuskil äärelinnas</div>
-        <div>+36584562</div>
+        <div onClick={()=> muudaN2itaTelYlemiste(!n2itaTelYlemiste)}>Ülemiste keskus</div>
+        <div>Suur-Sõjamäe 4</div>
+       {n2itaTelYlemiste && <div>+36584562</div>}
         <br/>
-        <div>Pärnu kontor</div>
-        <div>Kuskil rannas</div>
-        <div>+358456985</div>
+        <div onClick={()=> muudaN2itaTelTasku(!n2itaTelTasku)}>Tasku keskus</div>
+        <div>Turu 2</div>
+       {n2itaTelTasku && <div>+358456985</div>}
     </div>
   )
 }
--- uudised/src/pages/Meist.js
+++ uudised/src/pages/Meist.js
@@ -1,29 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Meist() {
+  const [kontakt, n2itaKontakt] = useState("");
+
   return (
     <div>
-        <div>See on meist leht, nähtav localhot:3000/meist aadressil</div>
-        <br/>
-        <div>Meie töötajad</div>
-        <br/>
-        <div>Kass Artur</div>
-        <div>+3584569545</div>
-        <div>Pealik</div>
-        <br/>
-        <div>Saabastega Kass</div>
-        <div>+358456985</div>
-        <div>Reporter</div>
-        <br/>
-        <div>Hunt Kriimsilm</div>
-        <div>+36584568445</div>
-        <div>Reklaam ja turundus</div>
-        <br/>
-        <div>Karupoeg Puhh</div>
-        <div>+3654854562</div>
-        <div>Toimetaja</div>
+      <div>See on meist leht, nähtav localhot:3000/meist aadressil</div>
+      <br />
+      <div>Meie töötajad</div>
+      <br />
+      <div>Kass Artur</div>
+      <div>Pealik</div>
+      <button onClick={() => n2itaKontakt("+3584569545")}>Võta ühendust</button>
+      <br /> <br />
+      <div>Saabastega Kass</div>
+      <div>Reporter</div>
+      <button onClick={() => n2itaKontakt("+36584568445")}>
+        Võta ühendust
+      </button>
+      <br /> <br />
+      <div>Hunt Kriimsilm</div>
+      <div>Reklaam ja turundus</div>
+      <button onClick={() => n2itaKontakt("+358456985")}>Võta ühendust</button>
+      <br /> <br />
+      <div>Karupoeg Puhh</div>
+      <div>Toimetaja</div>
+      <button onClick={() => n2itaKontakt("+3654854562")}>Võta ühendust</button>
+      <br /> <br />
+      {kontakt !== "" && <div>Tema kontakt: {kontakt}</div>}{" "}
     </div>
-  )
+  );
 }
 
-export default Meist
+export default Meist;
