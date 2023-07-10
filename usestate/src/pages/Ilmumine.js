a0c4d7a065313bfdf1ca4db81442ac598c804ddc
Triin <trinzaa@gmail.com>
2023-07-11 00:09:21 +0300

    useState Telia näide

---
+++ usestate/src/pages/Ilmumine.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+
+function Ilmumine() {
+    const [n2itaLisainfot, uuendaN2itaLisainfot] = useState(false);
+
+
+  return (
+    <div>
+        <div onClick={() => uuendaN2itaLisainfot(!n2itaLisainfot)}>
+        Sooduskoodi sisestamine ja makse arvutuskäik
+        {n2itaLisainfot === true && <span>A</span>}
+        {n2itaLisainfot === false && <span>V</span>}
+      </div>
+      { n2itaLisainfot === true && <div>Tellitavad tooted ja teenused
+          Lisatud teenused
+          Family pakett
+
+          Soodustus kuni 29.03.2023 
+          0,00 €/kuu
+
+          39,00 €/kuu
+
+          Videolaenutus
+
+          Sisaldub paketis
+          FOX NOW
+
+          Sisaldub paketis
+          HBO
+
+          Sisaldub paketis
+          National Geographic keskkond
+
+          Sisaldub paketis
+          Go3 Film / Paramount+
+
+          Sisaldub paketis
+          Lastenurk
+
+          Sisaldub paketis
+          Setanta Sportsi äpp
+
+          Sisaldub paketis
+          5 ekraani
+
+          Sisaldub paketis
+          Inspira +
+
+          Sisaldub paketis
+          Salvestamine
+
+          Sisaldub paketis
+      </div>}
+
+
+    </div>
+  )
+}
+
+export default Ilmumine
