3a2b36ca84e0f2fc1c4acc15502d93329a1ca484
Triin <trinzaa@gmail.com>
2023-07-13 02:23:36 +0300

    Arrayd: book titles sort & filter

--- kodutoo1/src/pages/Books.js
+++ kodutoo1/src/pages/Books.js
@@ -22,6 +22,33 @@ function Books() {
         updateBooks(books.slice());
     }
 
+    const sortBySecondLetter = () => {
+        books.sort((a,b) => a[1].localeCompare(b[1]), 'fi')
+        updateBooks(books.slice());
+
+    }
+
+    const sortByNumberOfWords = () => {
+        const sortedBooks = books.sort((a, b) => {
+            const aWordCount = a.split(' ').length;
+            const bWordCount = b.split(' ').length;
+            return aWordCount - bWordCount;
+          });
+          
+        updateBooks(sortedBooks.slice());
+
+    }
+    const sortByPenultimateLetter = () => {
+        const sortedBooks = books.sort((a, b) => {
+            const aPenultimateLetter = a[a.length - 2]; // Get the penultimate letter of string 'a'
+            const bPenultimateLetter = b[b.length - 2]; // Get the penultimate letter of string 'b'
+          
+            return aPenultimateLetter.localeCompare(bPenultimateLetter); // Compare the penultimate letters
+          });
+        updateBooks(sortedBooks.slice());
+
+    }
+
     const filterStartWithThe = () => {
         const result = books.filter(book => book.startsWith("The"));
         updateBooks(result);
@@ -42,22 +69,42 @@ function Books() {
         updateBooks(result);
     }
     
+    const filterThreeOrMoreWords = () => {
+        const result = books.filter((book) => {
+            const wordCount = book.split(' ').length; // Count the number of words in the book title
+            return wordCount >= 3; // Filter elements with three or more words
+        })
+        updateBooks(result);
+    }
+    
+    const filterPenultimateLetterC = () => {
+        const result = books.filter((book) => {
+            const penultimateLetter = book[book.length - 2];
+            return penultimateLetter.toLocaleLowerCase() === 'c'
+        })
+        updateBooks(result);
+    }
 
 
 
   return (
     <div>
-        {books.map((book) => <div key={book}>{book}</div> )}
-
+        {books.map((book) => <div key={book}>{book}</div> )} <br />
         <button onClick={reset}>Reset</button> <br />
         <button onClick={sortAZ}>Soorteeri A-Z</button>
         <button onClick={sortZA}>Soorteeri Z-A</button>
-        <button onClick={sortByTitleLenght}>Pealkirja pikkus</button>
-
+        <button onClick={sortByTitleLenght}>Sorteeri: pealkirja pikkus</button>
+        <button onClick={sortBySecondLetter}>Sorteeri: teine täht</button>
+        <button onClick={sortByNumberOfWords}>Sorteeri: sõnade arv</button>
+        <button onClick={sortByPenultimateLetter}>Sorteeri: eelviimane täht</button>
+        
+        <br /><br />
         <button onClick={filterStartWithThe}>Algab The-ga</button>
-        <button onClick={filterMiddleAnd}>Keskel on the</button>
+        <button onClick={filterMiddleAnd}>Keskel on and</button>
         <button onClick={filterLongerThan10}>Pikem kui 10 märki</button>
         <button onClick={filtershorterThan7}>Lühem kui 7 märki</button>
+        <button onClick={filterThreeOrMoreWords}>3 või rohkem sõna</button>
+        <button onClick={filterPenultimateLetterC}>Eelviimane täht on C</button>
 
 
 
