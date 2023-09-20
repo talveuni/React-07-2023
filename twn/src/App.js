dea09ba484afdf12eff99d44cee41608e0b790ba
Triin <trinzaa@gmail.com>
2023-09-20 23:11:18 +0300

    twn: upper menu

--- twn/src/App.js
+++ twn/src/App.js
@@ -3,14 +3,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 import List from "./pages/List";
 import ArticlePage from "./pages/ArticlePage";
+import { Button } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
+       <div className="upper_menu_bar">
+        <Button as={Link} to= {"/article/972d2b8a"} variant="light" className="menu_btn">Artikkel</Button>      
+        <Button as={Link} to= {"/list"} variant="light" className="menu_btn">Tabel</Button>      
+        </div>
       
       <div className="sidebar">
-        <Link to ="/article/972d2b8a"> <button>Artikkel</button></Link> <br /> <br />
-        <Link to ="/list"> <button>Tabel</button></Link>
+      ``<Button as={Link} to= {"/article/972d2b8a"} variant="light" className="menu_btn">Artikkel</Button> <br />     
+        <Button as={Link} to= {"/list"} variant="light" className="menu_btn">Tabel</Button> 
       
       </div>
       <Routes>
