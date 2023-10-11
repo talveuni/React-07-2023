f639b3d7c4cfec119a0ae41fac6923e4b7646852
Triin <trinzaa@gmail.com>
2023-10-11 10:31:55 +0300

    bridge - tricks

--- bridge-table/src/App.js
+++ bridge-table/src/App.js
@@ -1,46 +1,62 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import { BsSuitClubFill, BsSuitDiamondFill, BsSuitHeartFill, BsSuitSpadeFill } from "react-icons/bs";
-import { useState } from 'react';
-import { Table } from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import {
+  BsSuitClubFill,
+  BsSuitDiamondFill,
+  BsSuitHeartFill,
+  BsSuitSpadeFill,
+} from "react-icons/bs";
+import { useState } from "react";
+import { Button, Table } from "react-bootstrap";
 
 function App() {
-const [level, setLevel] = useState(1);
-const [trump, setTrump] = useState("");
-const [risk, setRisk] = useState("");
-const [tricks, setTricks] = useState([])
+  const [level, setLevel] = useState("");
+  const levels = [1, 2, 3, 4, 5, 6, 7];
+  const [trump, setTrump] = useState("");
+  const trumps = ["clubs", "diamonds", "hearts", "spades", "NT"];
+  const [risk, setRisk] = useState("");
+  const risks = ["No double", "Double", "Redouble"];
+  const [tricks, setTricks] = useState([]);
+  const [trick, setTrick] = useState("");
+  const [message, setMessage] = useState("")
 
-const generateTricks = (level) => {
+  const chooseLevel = (level) => {
+    setTrick("");
+    generateTricks(level);
+    setLevel(level);
+  };
 
-  const tricksRange = [];
-  const totalPlusTrics = 7-level
-  const totalMinusTrics = -level-6
-  
-  for (let i = totalMinusTrics; i <= totalPlusTrics; i++) {
-    if (i === 0) {
-      tricksRange.push("=");
-    } else {
-      tricksRange.push(i);
+  const generateTricks = (level) => {
+    const tricksRange = [];
+    const totalPlusTrics = 7 - level;
+    const totalMinusTrics = -level - 6;
+
+    for (let i = totalMinusTrics; i <= totalPlusTrics; i++) {
+      if (i === 0) {
+        tricksRange.push("=");
+      } else {
+        tricksRange.push(i);
+      }
     }
+
+    return tricksRange;
+  };
+
+  // Set the tricks state once when the component mounts
+  useState(() => {
+    setTricks(generateTricks(level));
+  }, [level]);
+
+  const calculateScore = () => {
+    setMessage("Mingi tähtis arvutus on toimunud")
+    
   }
 
-
-
-  
-return tricksRange
-}
-
-// Set the tricks state once when the component mounts
-useState(() => {
-  setTricks(generateTricks(level));
-}, [level]);
-
-
   return (
-    <div className='App'>
-        <div>
-          <h2>Bridge compensation score calculator</h2>
-          <br />
+    <div className="App">
+      <div>
+        <h2>Bridge compensation score calculator</h2>
+        <br />
         <Table>
           <thead>
             <tr>
@@ -50,180 +66,105 @@ useState(() => {
           </thead>
           <tbody>
             <tr>
-              <td >Level: </td>
-              <td className='choice_container'>
-              <label className='choice'>
-              <input className='level'
-                type="radio"
-                name="level"
-                value="1"
-                onChange={()=>setLevel(1)}
-              />
-              <span>1</span>
-              </label>
-
-              <label className='choice'>
-              <input className='level'
-                type="radio"
-                name="level"
-                value="2"
-                onChange={()=>setLevel(2)}
-              />
-              <span>2</span>
-              </label>
-              <label className='choice'>
-              <input className='level'
-                type="radio"
-                name="level"
-                value="3"
-                onChange={()=>setLevel(3)}
-              />
-              <span>3</span>
-              </label> <label className='choice'>
-              <input className='level'
-                type="radio"
-                name="level"
-                value="4"
-                onChange={()=>setLevel(4)}
-              />
-              <span>4</span>
-              </label> <label className='choice'>
-              <input className='level'
-                type="radio"
-                name="level"
-                value="5"
-                id="5"
-                checked={level === 5}
-                onChange={()=>setLevel(5)}
-              />
-              <span>5</span>
-              </label> <label className='choice'>
-              <input className='level'
-                type="radio"
-                name="level"
-                value="6"
-                onChange={()=>setLevel(6)}
-              />
-              <span>6</span>
-              </label> <label className='choice'>
-              <input className='level'
-                type="radio"
-                name="level"
-                value="7"
-                onChange={()=>setLevel(7)}
-              />
-              <span>7</span>
-              </label>
+              <td>Level: </td>
+              <td className="choice_container">
+                {levels.map((value) => (
+                  <label className={level === value ? "selected_choice" : "choice"} key={value}>
+                    <input
+                      className="level"
+                      type="radio"
+                      name="level"
+                       value={value.toString()}
+                      checked={level === value}
+                      onChange={() => chooseLevel(value)}
+                    />
+                    <span>{value}</span>
+                    
+                  </label>
+                ))}
               </td>
             </tr>
+
             <tr>
-              <td > Trump: </td>
-              <td className='choice_container'>
-              <label className='choice'>
-              <input className='trump'
-                type="radio"
-                name="trump"
-                value="clubs"
-                onChange={()=>setTrump("clubs")}
-              />
-              <BsSuitClubFill />
-              </label>
-
-              <label className='choice'>
-              <input className='trump'
-                type="radio"
-                name="trump"
-                value="diamonds"
-                onChange={()=>setTrump("diamonds")}
-              />
-              <BsSuitDiamondFill className='red'/>
-              </label>
-
-              <label className='choice'>
-              <input className='trump'
-                type="radio"
-                name="trump"
-                value="hearts"
-                onChange={()=>setTrump("hearts")}
-              />
-              <BsSuitHeartFill className='red'/>
-              </label>
-
-              <label className='choice'>
-              <input className='trump'
-                type="radio"
-                name="trump"
-                value="spades"
-                onChange={()=>setTrump("spades")}
-              />
-            <BsSuitSpadeFill/>
-            </label>
-
-            <label className='choice'>
-              <input className='trump'
-                type="radio"
-                name="trump"
-                value="NT"
-                onChange={()=>setTrump("NT")}
-              />
-            <span>NT</span>
-            </label>
+              <td>Trump: </td>
+              <td className="choice_container">
+                {trumps.map((value) => (
+                  <label className={trump === value ? "selected_choice" : "choice"} key={value}>
+                    <input
+                      className="trump"
+                      type="radio"
+                      name="trump"
+                      value={value}
+                      checked={trump === value}
+                      onChange={() => setTrump(value)}
+                    />
+                    {value==="clubs"&& <BsSuitClubFill /> }
+                    {value==="diamonds"&& <BsSuitDiamondFill className="red"/> }
+                    {value==="hearts"&& <BsSuitHeartFill className="red"/> }
+                    {value==="spades"&& <BsSuitSpadeFill /> }
+                    {value==="NT"&& <span>NT</span> }
+                
+                  </label>
+                ))}
               </td>
             </tr>
+           
             <tr>
               <td>Risk: </td>
-              <td className='choice_container'>
-                <label className='choice'>
-                  <p>No double</p>
-                  <input className='risk'
-                    type="radio"
-                    name="risk"
-                    value="no double"
-                    onChange={()=>setRisk("No double")}
-                  />
-                </label>
-                <label className='choice'>
-                  <p>Double</p>
-                  <input className='risk'
-                    type="radio"
-                    name="risk"
-                    value="double"
-                    onChange={()=>setRisk("Double")}
-                  />
-                </label>
-                <label className='choice'>
-                  <p>Redouble</p>
-                  <input className='risk'
-                    type="radio"
-                    name="risk"
-                    value="redouble"
-                    onChange={()=>setRisk("Redouble")}
-                  />
-                </label>
-                
-                <div className='risk'>No double</div>
-                <div className='risk'>Double</div>
-                <div className='risk'>Redouble</div>
+              <td className="choice_container">
+                {risks.map((value) => (
+                  <label className={risk === value ? "selected_choice" : "choice"} key={value}>
+                    <input
+                      className="risk"
+                      type="radio"
+                      name="risk"
+                      value={value}
+                      checked={risk === value}
+                      onChange={() => setRisk(value)}
+                    />
+                    <span>{value}</span>
+                    
+                  </label>
+                ))}
               </td>
+            </tr>
+            
+           
+            {
+              <tr>
+                <td>Tricks: </td>
+                <td className="choice_container">
+                  {generateTricks(level).map((value, index) => (
+                    <label className={value === trick ? "selected_choice" : "choice"} key={index}>
+                      <input
+                        className="trick"
+                        type="radio"
+                        name="trick"
+                        value={value.toString()}
+                        onChange={() => setTrick(value)}
+                      />
+                      {value}
+                    </label>
+                  ))}
+                </td>
+              </tr>
+            }
+            <tr>
+              <td colSpan={2}><div className="calc_btn"><Button onClick={calculateScore} variant="outline-secondary">Arvuta</Button></div></td>
+              
             </tr>
             <tr>
-              <td>Tricks: </td>
-              <td className='choice_container'>
-                {generateTricks(level).map((trick, index) => (
-                  <div key={index} className='choice'>
-                    {trick}
-                  </div>             
-                  ))}
-              </td>
+              <td>Score: </td>
+              <td> 
+                {message} </td>
             </tr>
-
           </tbody>
         </Table>
         <p>Level: {level}</p>
         <p>Trump: {trump}</p>
         <p>Risk: {risk}</p>
-        <p>Trics: </p>
-      </div>     
+        <p>Trics: {trick}</p>
+      </div>
     </div>
   );
 }
